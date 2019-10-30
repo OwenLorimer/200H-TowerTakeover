381f5c9b166967b2444c5cd1d09e4d078ec5979e
OwenLorimer <57195616+OwenLorimer@users.noreply.github.com>
2019-10-30 17:53:43 -0400

    Update lift.cpp

--- lift.cpp
+++ lift.cpp
@@ -17,6 +17,7 @@ struct RobotLift{
   pros::Controller rcLift;
   pros::Motor* liftL = nullptr;
   pros::Motor* liftR = nullptr;
+  pros::Motor* liftClaw = nullptr;
   pros::ADIButton* liftLim = nullptr;
 
   int liftState;
@@ -28,9 +29,10 @@ struct RobotLift{
 
 static RobotLift l1;
 
-void LiftInit(pros::Controller rcName, int ml, int mr, int bumper){
+void LiftInit(pros::Controller rcName, int ml, int mr, int bumper, mClaw){
   l1.liftL = new pros::Motor (ml);
   l1.liftR = new pros::Motor (mr);
+  l1.liftClaw = new pros::Motor (mClaw);
   l1.rcLift = rcName;
   l1.liftLim = new pros::ADIButton (bumper);
 }
@@ -61,8 +63,9 @@ void liftReset(){
   }
 }
 
-void runLift(int isRetracted, float liftPos){
 
+void runLift(int isRetracted, float liftPos){
+	
   if(liftPos < 1440 && l1.liftState != 1){
     if(rcGDV(l1.rcLift, R1)){
       lift(127);
@@ -86,8 +89,19 @@ void runLift(int isRetracted, float liftPos){
   else{
     liftTare();
   }
+if (rcGDV(l1.rcLift, L1)){
+  l1.liftClaw->move(127);
+}
+	
+else if (rcGDV(l1.rcLift, L2)){
+   l1.liftClaw->move(-127);
 }
 
+else{
+   l1.liftClaw->move(0);
+}
+
+	
 void rcLiftInput(){
   l1.liftState = l1.liftLim->get_value();
   float liftPos = fabs(((l1.liftL->get_position() + l1.liftR->get_position())/2));
